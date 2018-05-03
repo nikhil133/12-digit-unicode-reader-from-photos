c1e5718c01b9dc1f952e0f4515e34eec1f00ff5b
nikhil133 <nikn135@gmail.com>
2018-05-03 12:26:42 +0530

    Extract 12 digit unique id

---
+++ addhar_number.m
@@ -0,0 +1,96 @@
+function [ x ] = addhar_number( str )
+
+if ispc
+     replace(str,'/','\');
+elseif isunix
+     replace(str,'\','/');
+elseif ismac 
+     replace(str,'\','/');
+end     
+colorImage = imread(str);
+I = rgb2gray(colorImage);
+
+% Detect MSER regions.
+[mserRegions, mserConnComp] = detectMSERFeatures(I, ... 
+    'RegionAreaRange',[200 8000],'ThresholdDelta',1.5);
+mserStats = regionprops(mserConnComp, 'BoundingBox', 'Eccentricity', ...
+    'Solidity', 'Extent', 'Euler', 'Image');
+
+% Compute the aspect ratio using bounding box data.
+bbox = vertcat(mserStats.BoundingBox);
+w = bbox(:,3);
+h = bbox(:,4);
+aspectRatio = w./h;
+
+% Threshold the data to determine which regions to remove. These thresholds
+% may need to be tuned for other images.
+filterIdx = aspectRatio' > 3; 
+filterIdx = filterIdx | [mserStats.Eccentricity] > .995 ;
+filterIdx = filterIdx | [mserStats.Solidity] < .3;
+filterIdx = filterIdx | [mserStats.Extent] < 0.2 | [mserStats.Extent] > 0.9;
+filterIdx = filterIdx | [mserStats.EulerNumber] < -4;
+
+% Remove regions
+mserStats(filterIdx) = [];
+%mserRegions(filterIdx) = [];
+
+bboxes = vertcat(mserStats.BoundingBox);
+
+% Convert from the [x y width height] bounding box format to the [xmin ymin
+% xmax ymax] format for convenience.
+xmin = bboxes(:,1);
+ymin = bboxes(:,2);
+xmax = xmin + bboxes(:,3) - 1;
+ymax = ymin + bboxes(:,4) - 1;
+
+% Expand the bounding boxes by a small amount.
+expansionAmount = 0.02;
+xmin = (1-expansionAmount) * xmin;
+ymin = (1-expansionAmount) * ymin;
+xmax = (1+expansionAmount) * xmax;
+ymax = (1+expansionAmount) * ymax;
+
+% Clip the bounding boxes to be within the image bounds
+xmin = max(xmin, 1);
+size(xmin)
+ymin = max(ymin, 1);
+xmax = min(xmax, size(I,2));
+ymax = min(ymax, size(I,1));
+
+% Show the expanded bounding boxes
+expandedBBoxes = [xmin ymin xmax-xmin+1 ymax-ymin+1];
+%IExpandedBBoxes = insertShape(colorImage,'Rectangle',expandedBBoxes,'LineWidth',3);
+
+overlapRatio = bboxOverlapRatio(expandedBBoxes, expandedBBoxes);
+
+% Set the overlap ratio between a bounding box and itself to zero to
+% simplify the graph representation.
+n = size(overlapRatio,1); 
+overlapRatio(1:n+1:n^2) = 0;
+
+% Create the graph
+g = graph(overlapRatio);
+
+% Find the connected text regions within the graph
+componentIndices = conncomp(g);
+size(componentIndices)
+xmin = accumarray(componentIndices', xmin, [], @min);
+size(xmin)
+ymin = accumarray(componentIndices', ymin, [], @min);
+xmax = accumarray(componentIndices', xmax, [], @max);
+ymax = accumarray(componentIndices', ymax, [], @max);
+
+textBBoxes = [xmin ymin xmax-xmin+1 ymax-ymin+1];
+
+numRegionsInGroup = histcounts(componentIndices);
+textBBoxes(numRegionsInGroup == 1, :) = [];
+
+% Show the final text detection result.
+ocrtxt = ocr(I,textBBoxes);
+txt=cell2mat({ocrtxt.Text});
+pattern='\d{1,4}\s\d{1,4}\s\d{1,4}';
+x=string(regexp(txt,pattern,'match'));
+
+
+end
+
